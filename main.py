1e400f5dfe313fff2a46c3904b9609a71d7859b3
sunyating1 <1613152158@qq.com>
2025-09-08 11:30:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 weight_input = input("请输入重量：")
 if weight_input [-2:] in ['kg', 'KG']:
     pound = (eval(weight_input[0:-2])) * 2.2046
-    print(f"对应的英制重量为{pound:.3f}磅")
-elif weight_input [-2:] in ['pd']:
+    print("对应的英制重量为{:.3f}磅".format(pound))
+elif weight_input [-2:] in ['pd' ,'PD']:
     kilogram = (eval(weight_input[0:-2])) * 0.4535
-    print(f"对应的公制重量为{kilogram:.3f}千克")
+    print("对应的公制重量为{:.3f}千克".format(kilogram))
 else:
     print("输入格式错误")
